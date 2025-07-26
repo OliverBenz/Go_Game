b159b3c4ae4c6a80edc93e0b42fe7b27121b3e15
Oliver Benz <oliver.benz@outlook.com>
2025-07-26 09:20:49 +0200

    Dynamic board drawing. 1px off in corner.

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -3,9 +3,15 @@
 #include <SDL_image.h>
 #include <iostream>
 
-constexpr int SCREEN_WIDTH = 900;
-constexpr int SCREEN_HEIGHT = 900;
+constexpr int GAME_SIZE = 13;
 
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 800;
+
+// Make space for GAME_SIZE many stones exactly
+constexpr int BOARD_SIZE = static_cast<int>(std::min(SCREEN_WIDTH, SCREEN_HEIGHT) / GAME_SIZE) * GAME_SIZE;
+constexpr int STONE_SIZE = BOARD_SIZE / GAME_SIZE; // TODO: Could still be a pixel off due to rounding int division
+                                                              //
 SDL_Texture* load_texture(const char* path, SDL_Renderer* renderer){
     SDL_Surface* surface = IMG_Load(path);
     if (!surface) {
@@ -21,8 +27,48 @@ SDL_Texture* load_texture(const char* path, SDL_Renderer* renderer){
     return tex;
 }
 
-int main() {
+// TODO: Move to own class and only construct board textures once.
+// Draw function should not include texture loading, coordinate construction, size calc, etc
+void drawBoard(SDL_Renderer* renderer) {
+    static constexpr int LW = 2;
+    static constexpr int DRAW_STEP_SIZE = STONE_SIZE/2;
 
+    static constexpr int coordStart    = DRAW_STEP_SIZE;             //!< (x,y) starting coordinate of lines
+    static constexpr int coordEnd      = BOARD_SIZE-DRAW_STEP_SIZE;  //!< (x,y) ending coordinate of lines
+    static constexpr int effBoardWidth = coordEnd - coordStart;      //!< Board width between outer lines
+
+
+    SDL_Rect dest_board{.x=0, .y=0, .w=BOARD_SIZE, .h=BOARD_SIZE};
+    SDL_SetRenderDrawColor(renderer, 220, 179, 92, 255);
+    SDL_RenderFillRect(renderer, &dest_board);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    for (int i = 0; i != GAME_SIZE; ++i) {
+        SDL_Rect dest_line {
+            .x = coordStart,
+            .y = coordStart + i * STONE_SIZE,
+            .w = effBoardWidth,
+            .h = LW
+        };
+        SDL_RenderFillRect(renderer, &dest_line);
+
+        //SDL_RenderDrawLine(renderer, DRAW_STEP_SIZE, pos, BOARD_SIZE-DRAW_STEP_SIZE, pos);
+    }
+    for (int i = 0; i != GAME_SIZE; ++i) {
+        SDL_Rect dest_line {
+            .x = coordStart + i * STONE_SIZE,
+            .y = coordStart,
+            .w = LW,
+            .h = effBoardWidth
+        };
+        SDL_RenderFillRect(renderer, &dest_line);
+
+        // const int pos = i*STONE_SIZE - DRAW_STEP_SIZE;
+        // SDL_RenderDrawLine(renderer, pos, DRAW_STEP_SIZE, pos, BOARD_SIZE-DRAW_STEP_SIZE);
+    }
+}
+
+int main() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "Failed to initialize SDL.\n";
         return -1;
@@ -42,43 +88,58 @@ int main() {
         return -1;
     }
 
-    SDL_Texture* board = load_texture("/home/oliver/Data/dev/Go_Game/assets/Blank_Go_board.webp", renderer);
     SDL_Texture* black = load_texture("/home/oliver/Data/dev/Go_Game/assets/black.png", renderer);
     SDL_Texture* white = load_texture("/home/oliver/Data/dev/Go_Game/assets/white.png", renderer);
-    if(!board || !black) {
+    if(!white || !black) {
         return -1;
     }
 
+    // int board_width, board_height;
+    // SDL_QueryTexture(board, nullptr, nullptr, &board_width, &board_height); 
+    // assert(board_width == board_height);
 
-    int black_width, black_height;
-    SDL_QueryTexture(black, nullptr, nullptr, &black_width, &black_height); 
+    // int black_width, black_height;
+    // SDL_QueryTexture(black, nullptr, nullptr, &black_width, &black_height); 
 
-    int white_width, white_height;
-    SDL_QueryTexture(white, nullptr, nullptr, &white_width, &white_height); 
-    assert(white_width == black_width);
-    assert(white_height == black_height);
+    // int white_width, white_height;
+    // SDL_QueryTexture(white, nullptr, nullptr, &white_width, &white_height); 
+    // assert(white_width == black_width);
+    // assert(white_height == black_height);
+
 
     SDL_Rect dest_white;
-    dest_white.w = black_width;
-    dest_white.h = black_height;
-    dest_white.x = (SCREEN_WIDTH - black_width) / 2 + black_width;
-    dest_white.y = (SCREEN_HEIGHT - black_height) / 2;
+    dest_white.w = STONE_SIZE;
+    dest_white.h = STONE_SIZE;
+    dest_white.x = (SCREEN_WIDTH - STONE_SIZE) / 2 + STONE_SIZE;
+    dest_white.y = (SCREEN_HEIGHT - STONE_SIZE) / 2;
 
     SDL_Rect dest_black;
-    dest_black.w = black_width;
-    dest_black.h = black_height;
-    dest_black.x = (SCREEN_WIDTH - black_width) / 2;
-    dest_black.y = (SCREEN_HEIGHT - black_height) / 2;
+    dest_black.w = STONE_SIZE;
+    dest_black.h = STONE_SIZE;
+    dest_black.x = (SCREEN_WIDTH - STONE_SIZE) / 2;
+    dest_black.y = (SCREEN_HEIGHT - STONE_SIZE) / 2;
 
     SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, board, nullptr, nullptr);
+    drawBoard(renderer);
     SDL_RenderCopy(renderer, black, nullptr, &dest_black);
     SDL_RenderCopy(renderer, white, nullptr, &dest_white);
+
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(5000);
+    char inp = ' ';
+    do {
+        SDL_RenderClear(renderer);
+        drawBoard(renderer);
+        SDL_RenderCopy(renderer, black, nullptr, &dest_black);
+        SDL_RenderCopy(renderer, white, nullptr, &dest_white);
 
-    SDL_DestroyTexture(board);
+        SDL_RenderPresent(renderer);
+        inp = ' ';
+        std::cin >> inp;
+    } while(inp != 'e');
+
+
+    // SDL_DestroyTexture(board);
     SDL_DestroyTexture(black);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
