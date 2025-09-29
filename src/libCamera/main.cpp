ab76a8a51100cbd1b9a647e51ac1a4f064c8f99b
Oliver Benz <oliver.benz@outlook.com>
2025-09-29 15:33:30 +0200

    Camera: Stupid attempts to detect/center board and detect stones.

--- src/libCamera/main.cpp
+++ src/libCamera/main.cpp
@@ -18,8 +18,11 @@ public:
     }
 
 private:
-    static constexpr int hmin=13, smin=44, vmin=172;
+    static constexpr int hmin=13, smin=44, vmin=194;
     static constexpr int hmax=37, smax=197, vmax=255;
+
+    //! Shifting vmin around the base by this amound shoud allow to find most boards.
+    static constexpr int vminRange = 20;
 };
 
 class MaskTesterHSV{
@@ -35,7 +38,7 @@ public:
         cv::createTrackbar("V Max", "Mask", &vmax, 255);
     }
     // Apply HSV mask to one image
-    cv::Mat process(const cv::Mat& img) {
+    cv::Mat processSimpleMask(const cv::Mat& img) {
         cv::Mat hsv, mask, result;
         cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
         cv::inRange(hsv, cv::Scalar(hmin, smin, vmin), cv::Scalar(hmax, smax, vmax), mask);
@@ -43,7 +46,110 @@ public:
         return result;
     }
 
-    int hmin=13, smin=44,  vmin=172;
+    cv::Mat processAndWarp(const cv::Mat& img) {
+        cv::Mat hsv, mask;
+        cv::cvtColor(img, hsv, cv::COLOR_BGR2HSV);
+        cv::inRange(hsv, cv::Scalar(hmin, smin, vmin),
+                        cv::Scalar(hmax, smax, vmax), mask);
+
+        // --- find board contour ---
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        if (contours.empty()) {
+            return img.clone(); // fallback
+        }
+
+        // largest contour = board
+        double maxArea = 0;
+        int maxIdx = -1;
+        for (size_t i = 0; i < contours.size(); i++) {
+            double area = cv::contourArea(contours[i]);
+            if (area > maxArea) {
+                maxArea = area;
+                maxIdx = (int)i;
+            }
+        }
+
+        std::vector<cv::Point> approx;
+        cv::approxPolyDP(contours[maxIdx], approx,
+                        0.02 * cv::arcLength(contours[maxIdx], true), true);
+
+        if (approx.size() != 4) {
+            // not a quad, just show mask overlay
+            cv::Mat debug;
+            cv::cvtColor(mask, debug, cv::COLOR_GRAY2BGR);
+            cv::drawContours(debug, contours, maxIdx, cv::Scalar(0,255,0), 2);
+            return debug;
+        }
+
+        // --- order the 4 corners consistently ---
+        // compute centroid
+        cv::Point2f center(0,0);
+        for (auto& p : approx)
+            center +=  cv::Point2f(p);
+        center *= (1.0 / approx.size());
+
+        std::vector<cv::Point2f> ordered(4);
+        for (auto& p : approx) {
+            if      (p.x < center.x && p.y < center.y) ordered[0] = p; // top-left
+            else if (p.x > center.x && p.y < center.y) ordered[1] = p; // top-right
+            else if (p.x > center.x && p.y > center.y) ordered[2] = p; // bottom-right
+            else                                       ordered[3] = p; // bottom-left
+        }
+
+        // --- target rectangle size ---
+        int outSize = 800; // you can pick board resolution
+        std::vector<cv::Point2f> target = {
+            {0,0}, {outSize-1,0}, {outSize-1,outSize-1}, {0,outSize-1}
+        };
+
+        // --- perspective transform ---
+        cv::Mat M = cv::getPerspectiveTransform(ordered, target);
+        cv::Mat warped;
+        cv::warpPerspective(img, warped, M, cv::Size(outSize, outSize));
+
+        return warped;
+    }
+
+
+    // TODO: Stone detection is bad. I need a different mask to find stones and their colour.
+    cv::Mat processWithStones(const cv::Mat& img) {
+        cv::Mat warped = processAndWarp(img);
+        auto stones = detectStones(warped);
+        cv::Mat withStones = drawStones(warped, stones);
+    
+        return withStones;
+    }
+
+    std::vector<cv::Vec3f> detectStones(const cv::Mat& warpedBoard) {
+        cv::Mat gray;
+        cv::cvtColor(warpedBoard, gray, cv::COLOR_BGR2GRAY);
+        cv::medianBlur(gray, gray, 5);
+
+        std::vector<cv::Vec3f> circles;
+        cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1,
+                        20,   // minDist between stone centers (tune: ~cell size)
+                        100, 30, 10, 30); // param1, param2, minRadius, maxRadius
+
+        return circles;
+    }
+    
+    cv::Mat drawStones(const cv::Mat& img, const std::vector<cv::Vec3f>& stones) {
+        cv::Mat result = img.clone();
+        for (size_t i = 0; i < stones.size(); i++) {
+            cv::Point center(cvRound(stones[i][0]), cvRound(stones[i][1]));
+            int radius = cvRound(stones[i][2]);
+            // Circle outline
+            cv::circle(result, center, radius, cv::Scalar(0, 255, 0), 2);
+            // Center dot
+            cv::circle(result, center, 2, cv::Scalar(0, 0, 255), 3);
+        }
+        return result;
+    }
+
+
+    int hmin=13, smin=44,  vmin=187; // or vmin 186
     int hmax=37, smax=197, vmax=255;
 };
 
@@ -96,7 +202,7 @@ int main() {
     while (true) {
         std::vector<cv::Mat> processed;
         for (auto& img : images) {
-            processed.push_back(testMask.process(img));
+            processed.push_back(testMask.processWithStones(img));
         }
         cv::Mat grid = makeGrid(processed);
         cv::imshow("Masks", grid);
