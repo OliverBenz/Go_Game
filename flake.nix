0517fbfa838a52b436b92b91e182699d8580e6b4
Oliver Benz <oliver.benz@outlook.com>
2025-11-30 10:42:23 +0100

    Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Go_Game development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.gcc
+          pkgs.cmake
+          pkgs.SDL2
+          pkgs.SDL2_image
+          pkgs.glfw
+          pkgs.opencv
+          pkgs.zlib
+          pkgs.libpng
+          pkgs.libjpeg
+          pkgs.libtiff
+          pkgs.libwebp
+        ];
+      };
+    };
+}
