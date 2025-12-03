1bed6eeca54e3f6595dd6dd6debe8bfcbff1a55d
Oliver Benz <oliver.benz@outlook.com>
2025-12-03 15:08:01 +0100

    Core: Start proper core setup.

---
+++ src/core/sgfHandler.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "coordinate.hpp"
+
+namespace go::core {
+
+//! Convert sgf code to core game coordinate.
+Coord fromSGF(const std::string& s);
+
+//! Convert core game coordinate to sgf code.
+std::string toSGF(Coord c);
+
+
+}
