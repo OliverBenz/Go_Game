bbaa5fbea5b75cee8964f188346d139565d4ec56
Oliver Benz <oliver.benz@outlook.com>
2025-12-07 11:25:05 +0100

    Test: Add first non-functioning tests.

---
+++ tests/core/game.gtest.cpp
@@ -0,0 +1,44 @@
+#include "core/game.hpp"
+
+#include <gtest/gtest.h>
+#include <thread>
+
+namespace go::gtest {
+
+// TODO: Verify board state after every place 
+TEST(Game, BoardUpdate) {
+    Game game;
+    std::thread gameThread([&]{
+        game.run();
+    })
+
+    // Setup ?koseki?
+    game.pushEvent(PutStoneEvent{0u, 1u});
+    game.pushEvent(PutStoneEvent{0u, 2u});
+    game.pushEvent(PutStoneEvent{1u, 0u});
+    game.pushEvent(PutStoneEvent{1u, 3u});
+    game.pushEvent(PutStoneEvent{2u, 1u});
+    game.pushEvent(PutStoneEvent{2u, 2u});
+    game.pushEvent(PutStoneEvent{1u, 2u});
+
+    // White takes 
+    game.pushEvent(PutStoneEvent{1u, 1u});
+
+    // Black cannot take (repeating board state)
+    game.pushEvent(PutStoneEvent{1u, 2u});
+
+    // Black plays somewhere else
+    game.pushEvent(PutStoneEvent{5u, 5u});
+
+    // White plays somewhere else
+    game.pushEvent(PutStoneEvent{5u, 6u});
+
+    // Black takes back
+    game.pushEvent(PutStoneEvent{1u, 2u});
+
+
+    game.pushEvent(ShutdownEvent{});
+    gameThread.join();
+}
+
+}
