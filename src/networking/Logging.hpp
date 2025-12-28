c327ef179d52b99b7fb271da528541a12af5a2a9
Oliver Benz <oliver.benz@outlook.com>
2025-12-28 13:39:33 +0100

    Network: Add logging.

---
+++ src/networking/Logging.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Logger/Logger.hpp"
+
+namespace go::networking {
+
+//! Returns the logger instance based on the set up configuration.
+Logging::Logger Logger();
+
+} // namespace go::ui
