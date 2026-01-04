607dac2fb1a9dd862213f6c1050a12460ea31ca6
Oliver Benz <oliver.benz@outlook.com>
2026-01-04 16:03:50 +0100

    Network.GTest: New test setup.

--- tests/network/server.gtest.cpp
+++ tests/network/server.gtest.cpp
@@ -3,6 +3,8 @@
 #include "network/protocol.hpp"
 
 #include <gtest/gtest.h>
+#include <chrono>
+#include <thread>
 
 namespace go::gtest {
 
@@ -16,18 +18,27 @@ TEST(Networking, Server) {
 
 	client1.connect("127.0.0.1");
 	client2.connect("127.0.0.1");
+	
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
 	EXPECT_TRUE(client1.send(network::NwPutStoneEvent{1u, 2u}));
 	EXPECT_TRUE(client2.send(network::NwChatEvent{"Heyo this is my chat message"}));
 
-	const auto response3 = client1.read();
-	const auto response4 = client2.read();
+	EXPECT_TRUE(client1.send(network::NwPutStoneEvent{1u, 2u}));
+	EXPECT_TRUE(client1.send(network::NwPassEvent{}));
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+	const auto c1Session = client1.read();
+	const auto c2Session = client2.read();
+	
+	std::cerr << "Client 1 received:\n\t" << c1Session << "\n";
+	std::cerr << "Client 2 received:\n\t" << c2Session << "\n";
 
 	client1.disconnect();
 	client2.disconnect();
 
-	std::cerr << "Client 1 received:\n\t" << response3 << "\n";
-	std::cerr << "Client 2 received:\n\t" << response4 << "\n";
+	
 }
 
 } // namespace go::gtest
