d0f9a24480707efa0775f35fbd4feca614df87a0
Oliver Benz <oliver.benz@outlook.com>
2026-01-11 15:21:21 +0100

    Server: Keep running until quit.

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,11 +2,21 @@
 
 #include "gameServer.hpp"
 
-#include <thread>
+#include <iostream>
 
 int main(int, char**) {
 	go::server::GameServer server;
-	server.start(); // Uncomment when running the server as a standalone process.
+	server.start();
 
+	// NOTE: Can extend to allow more commands
+	// Keep the server process alive until stdin closes or quit command.
+	std::string line;
+	while (std::getline(std::cin, line)) {
+		if (line == "quit" || line == "exit") {
+			break;
+		}
+	}
+
+	server.stop();
 	return 0;
 }
