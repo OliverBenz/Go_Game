163576761a9e577d2fc924cd80608f7623ffd89c
Oliver Benz <oliver.benz@outlook.com>
2026-01-24 11:15:20 +0100

    Data: Implement new data library.

---
+++ src/libData/include/data/position.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "data/board.hpp"
+#include "data/clock.hpp"
+#include "data/player.hpp"
+
+namespace go {
+
+enum class GameStatus {
+	Idle,   //!< Nothing happening.
+	Ready,  //!< Players ready to play.
+	Active, //!< Game being played.
+	Done    //!< Game over.
+};
+
+//! Current game position.
+struct Position {
+	Player toMove; //!< Current player to make a move.
+	Board board;   //!< Go Board.
+}
+
+} // namespace go
