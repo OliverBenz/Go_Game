f8d494d072da55f5aa3dfe520ce2f5ab18cb0211
Oliver Benz <oliver.benz@outlook.com>
2026-01-24 11:15:20 +0100

    Data: Implement new data library.

---
+++ src/libData/board.cpp
@@ -0,0 +1,45 @@
+#include "data/board.hpp"
+
+#include <cassert>
+
+namespace go {
+
+Board::Board(std::size_t size) : m_size(size), m_board(size * size, Stone::Empty) {
+}
+
+std::size_t Board::size() const {
+	return m_size;
+}
+
+bool Board::place(Coord c, Stone value) {
+	assert(c.x < m_size && c.y < m_size); // Game should verify valid coordinate.
+	assert(value != Stone::Empty);        // Use remove
+
+	if (isEmpty(c)) {
+		m_board[c.y * m_size + c.x] = value;
+		return true;
+	}
+	return false;
+}
+
+bool Board::remove(Coord c) {
+	assert(c.x < m_size && c.y < m_size); // Game should verify valid coordinate.
+
+	if (!isEmpty(c)) {
+		m_board[c.y * m_size + c.x] = Stone::Empty;
+		return true;
+	}
+	return false;
+}
+
+Board::Stone Board::get(Coord c) const {
+	assert(c.x < m_size && c.y < m_size); // Game should verify valid coordinate.
+	return m_board[c.y * m_size + c.x];
+}
+
+bool Board::isEmpty(Coord c) const {
+	assert(c.x < m_size && c.y < m_size); // Game should verify valid coordinate.
+	return get(c) == Stone::Empty;
+}
+
+} // namespace go
---
+++ src/libData/include/data/board.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "player.hpp"
+
+#include <cassert>
+#include <vector>
+
+namespace go {
+
+//! Coordinate pair for the board.
+struct Coord {
+	unsigned x;
+	unsigned y;
+};
+
+//! A physical go board of arbitrary size.
+class Board {
+public:
+	enum class Stone { Empty, Black, White };
+
+	Board(std::size_t size);
+
+	std::size_t size() const; //!< Size of the board.
+
+	bool place(Coord c, Stone value); //!< Try to place a stone at the given coordinate. False if not free.
+	bool remove(Coord c);             //!< Remove the stone at the given coordinate. False if already free.
+	Stone get(Coord c) const;         //!< Get the stone at the given position.
+	bool isEmpty(Coord c) const;      //!< True if the given coordinate is empty.
+
+private:
+	std::size_t m_size{0u};       //!< Board size (typically 9, 13, 19)
+	std::vector<Stone> m_board{}; //!< Board data.
+};
+
+//! Maps a player color to a stone color.
+inline constexpr Board::Stone toStone(const Player player) {
+	assert(player == Player::Black || player == Player::White);
+	return player == Player::White ? Board::Stone::White : Board::Stone::Black;
+}
+
+} // namespace go
