3ef5d81a0b48c6badfcdae47d3ee2e2553ab3874
Oliver Benz <oliver.benz@outlook.com>
2026-01-31 17:00:55 +0100

    Scripts: Explicit parameters in buid script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,6 @@
 echo "Entering Development Environment"
 echo "Using configuration: $configuration"
 nix develop .. --command bash -c "
-  cmake -S .. -B ../build -DCMAKE_BUILD_TYPE=$configuration
+  cmake -S .. -B ../build -DCMAKE_BUILD_TYPE=$configuration -DBUILD_TESTS=True -DBUILD_EXAMPLES=True
   cmake --build ../build -j
 "
