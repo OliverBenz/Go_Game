3c5dd3e0f5a2f1f46a336eae797fd14f39712562
Oliver Benz <oliver.benz@outlook.com>
2026-01-31 21:58:11 +0100

    App: Signal in sessionManager preventing signal deadlocks.

--- src/App/include/app/position.hpp
+++ src/App/include/app/position.hpp
@@ -10,11 +10,11 @@ namespace go::app {
 
 class Position {
 public:
-	Position(EventHub& hub); //!< Hub allows to signal listeners on interesting updates.
+	Position() = default;
 
 	void reset(std::size_t boardSize);                 //!< Reset the position to some default data.
-	void init(const gameNet::ServerGameConfig& event); //!< Initialize the given position.
-	void apply(const gameNet::ServerDelta& delta);     //!< Apply a delta to the current position if ok.
+	bool init(const gameNet::ServerGameConfig& event); //!< Initialize the given position. Returns true if it changed state.
+	bool apply(const gameNet::ServerDelta& delta);     //!< Apply a delta to the current position if ok.
 	void setStatus(GameStatus status);                 //!< Update the status.
 
 	const Board& getBoard() const;
@@ -23,16 +23,12 @@ public:
 
 private:
 	bool isDeltaApplicable(const gameNet::ServerDelta& delta); //!< Check if the delta is ok to use for the position update.
-	void updatePosition(const gameNet::ServerDelta& delta);    //!< Update the position based on a server delta.
-	void signalOnAction(gameNet::ServerAction action);         //!< Signal listeners. Call after applyDelta.
 
 private:
 	unsigned m_moveId{0};                  //!< Last move id in game.
 	GameStatus m_status{GameStatus::Idle}; //!< Current status of the game.
 	Player m_player{Player::Black};
 	Board m_board{9u};
-
-	EventHub& m_eventHub; //!< Send events when position changes.
 };
 
 } // namespace go::app
