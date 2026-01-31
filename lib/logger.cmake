d375b8629715070b5b0acfa7b391b090725e4d09
Oliver Benz <oliver.benz@outlook.com>
2026-01-31 16:39:16 +0100

    Lib: Update Logger version.

--- lib/logger.cmake
+++ lib/logger.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
   Logger
   GIT_REPOSITORY https://github.com/OliverBenz/Logger.git
-  GIT_TAG        955d8c4f97b837af25c5da4ae28cb8ef1170e2ce
+  GIT_TAG        8e8f8e4a61c13ae370b5295775f92b14920bbc1a
 )
 set(LOGGER_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(LOGGER_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
