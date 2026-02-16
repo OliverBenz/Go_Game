6bb64662655d7c25872491c412c720f7dea1c064
Oliver Benz <oliver.benz@outlook.com>
2026-02-16 08:34:13 +0100

    Camera: Move boardFinder to own file.

---
+++ src/Camera/include/camera/boardFinder.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "camera/debugVisualizer.hpp"
+
+#include <opencv2/core/mat.hpp>
+
+namespace go::camera {
+
+struct WarpResult {
+    cv::Mat warped; //!< Image warped to fit the rough board contour. 
+    cv::Mat H;      //!< Homography used to apply the rough warping.
+};
+
+//! Detect rough Go board outline in an image and warp to center the board. Cut out background
+//! \param [in] image Original unwarped image of a Go board.
+//! \note       In the resulting warped image, it is not defined what exactly the border is. This is done in the second step (rectifyImage).  
+WarpResult warpToBoard(const cv::Mat& image, DebugVisualizer* debugger = nullptr);
+
+
+}
--- src/Camera/include/camera/rectifier.hpp
+++ src/Camera/include/camera/rectifier.hpp
@@ -26,18 +26,4 @@ struct BoardGeometry{
 //! \returns    Image showing Go board in a top-down view with the background cut out and a slight padding around the outermost edges(to not cut off stones at the edges). 
 BoardGeometry rectifyImage(const cv::Mat& image, DebugVisualizer* debugger = nullptr);
 
-
-namespace internal {
-
-struct WarpResult {
-    cv::Mat warped; //!< Image warped to fit the rough board contour. 
-    cv::Mat H;      //!< Homography used to apply the rough warping.
-};
-
-//! Detect rough Go board outline in an image and warp to center the board. Cut out background
-//! \param [in] image Original unwarped image of a Go board.
-//! \note       In the resulting warped image, it is not defined what exactly the border is. This is done in the second step (rectifyImage).  
-WarpResult warpToBoard(const cv::Mat& image, DebugVisualizer* debugger = nullptr);
-
-}
 }
